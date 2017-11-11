112c2d6d1c758afb56fb10ba3a3a301fa2a0616d
Charlie Campanella <CharlieCamp98@gmail.com>
2017-11-11 04:34:24 -0500

    Update pre.js

--- test/pre.js
+++ test/pre.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 if (process.env.NODE_ENV !== "development") {
   throw new Error("Environment must be in development.")
-} else if (process.env.DB_NAME === "giphtly-production-temp") {
+} else if (process.env.DB_NAME === "production-db") {
   throw new Error("DO NOT RUN TESTS ON PRODUCTION DATABASE!!!!!!!")
 }
 const environment = require('../app/environment.js')
